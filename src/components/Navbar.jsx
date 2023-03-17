c5008616cf3f48a6a4dce3649eabac00c6a71d98
NureniJamiu <nurenijamiu951@gmail.com>
2023-03-17 22:20:23 +0100

    feat: add toggle theme functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import React from "react";
 import logo from "../assets/logo.png";
+import { BsSunFill, BsMoonFill } from "react-icons/bs";
 
 const Navbar = () => {
+  const [isActive, setIsActive] = useState("");
+
   return (
     <nav className="border-b px-24 py-3 text-slate-800">
       <div className="flex justify-between items-center">
@@ -29,6 +33,30 @@ const Navbar = () => {
           >
             Contact
           </a>
+
+          {/* TOGGLE THEME MODE */}
+          <div className="flex gap-5 bg-[#486284] border-2 border-[#325078]  px-3 py-2 rounded-full text-white">
+            <span
+              className={`cursor-pointer ${
+                isActive === "sun" && "text-blue-400"
+              }`}
+              title="light mode"
+              onClick={() => setIsActive("sun")}
+            >
+              <BsSunFill />
+            </span>
+            <span
+              className={`cursor-pointer ${
+                isActive === "moon" && "text-blue-400"
+              }`}
+              title="dark mode"
+              onClick={() => setIsActive("moon")}
+            >
+              <BsMoonFill />
+            </span>
+          </div>
+        </div>
+        <div className="flex gap-2 text-sm">
           <a
             href="#"
             className="border-b-2 border-b-white hover:border-b-2 hover:text-blue-600 hover:border-b-blue-600 py-2 transition duration-150 ease-in-out"
