b4a87d655ec1462989e11fa0000c4986bb87a6c5
NureniJamiu <nurenijamiu951@gmail.com>
2023-03-17 21:46:49 +0100

    style: set page bg to white & font to poppins

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Navbar } from "./components";
 function App() {
   return (
-    <div>
+    <div className="bg-white font-Poppins">
       <div>
         <Navbar />
       </div>
