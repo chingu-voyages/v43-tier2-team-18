79777c16e1973fe7a8f62235889cd8ac1582ae67
NureniJamiu <nurenijamiu951@gmail.com>
2023-03-17 21:45:13 +0100

    import & render navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Navbar } from "./components";
 function App() {
   return (
-    <div className="px-24">
-      <div className="text-blue-600 text-center font-bold text-xl">
-        V43-TIER2-TEAM-18
+    <div>
+      <div>
+        <Navbar />
       </div>
     </div>
   );
