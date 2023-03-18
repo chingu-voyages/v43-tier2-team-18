9aab1a9a0655b0c18a18c45513c014e7c94a5303
NureniJamiu <nurenijamiu951@gmail.com>
2023-03-18 21:21:24 +0100

    feat: add layout pattern to house all sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { Navbar, Footer } from "./components";
+import { Layout, Main } from "./components";
 function App() {
   return (
     <div className="relative bg-white font-Poppins dark:bg-black dark:text-gray-100">
-      <div>
-        <Navbar />
-        <Footer />
-      </div>
+      <Layout>
+        <Main />
+      </Layout>
     </div>
   );
 }
