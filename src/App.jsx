4727887804afcad53dc757255ee48da8095237f9
NureniJamiu <nurenijamiu951@gmail.com>
2023-03-18 20:03:19 +0100

    import & render footer component on page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Navbar } from "./components";
+import { Navbar, Footer } from "./components";
 function App() {
   return (
     <div className="relative bg-white font-Poppins dark:bg-black dark:text-gray-100">
       <div>
         <Navbar />
+        <Footer />
       </div>
     </div>
   );
