b1d76f598fb28ff269c4baaa0a8356c51dfba0ce
NureniJamiu <nurenijamiu951@gmail.com>
2023-03-22 14:08:55 +0100

    add destination info page

---
+++ src/components/DestinationInfo.jsx
@@ -0,0 +1,11 @@
+import { TabsSection } from "../components";
+
+const DestinationInfo = () => {
+  return (
+    <div>
+      <TabsSection />
+    </div>
+  );
+};
+
+export default DestinationInfo;
