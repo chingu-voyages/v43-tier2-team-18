0408ef467ed95dc5d29101e1863b69b17bf30c7e
NureniJamiu <nurenijamiu951@gmail.com>
2023-03-22 14:08:37 +0100

    add mock data

---
+++ src/data.js
@@ -0,0 +1,29 @@
+export const highlights = [
+  "Visit the stunning city of Geneva, located on the shore of Lake Geneva, and discover its historic landmarks and museums.",
+  "Explore the charming village of Zermatt, located at the base of the Matterhorn, and enjoy the stunning views of the Alps",
+  "Take a scenic train ride on the Bernina Express, one of the most beautiful train journeys in the world, and admire the stunning landscapes of the Swiss Alps.",
+  "Take a scenic train ride on the Bernina Express, one of the most beautiful train journeys in the world, and admire the stunning landscapes of the Swiss Alps.",
+  "Visit the picturesque town of Lucerne, located on the shore of Lake Lucerne, and explore its ancient bridges, churches, and museums.",
+  "Enjoy a day of relaxation on the stunning shores of Lake Geneva or Lake Lucerne.",
+];
+
+export const priceIncludes = [
+  "Accommodation in a 4-star hotel with breakfast.",
+  "Private air-conditioned transportation throughout the tour.",
+  "English-speaking tour guide",
+  "Admission fees to all attractions",
+  "Bottled water and refreshing towels during the tour",
+  "2 lunches and 1 dinner during the tour",
+];
+
+export const priceExcludes = [
+  "International and domestic airfare.",
+  "Personal expenses (such as souvenirs and additional meals).",
+  "Travel insurance.",
+];
+
+export const complimentaries = [
+  "Complimentary pickup and drop-off from your hotel in Switzerland.",
+  "Free Wi-Fi in the vehicle during the tour.",
+  "A small souvenir as a token of our appreciation for choosing our tour.",
+];
