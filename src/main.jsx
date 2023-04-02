6e8230740ab4ae819884da52ae69337964ac8d44
NureniJamiu <nurenijamiu951@gmail.com>
2023-04-02 12:30:26 +0100

    add login & signup route paths

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
-import { DestinationInfo, LayoutRoute, Main } from "./components";
+import { LayoutRoute } from "./components";
+import { DestinationInfo, Login, Signup } from "./pages";
 
+import "./index.css";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <LayoutRoute />
+    path: "/",
+    element: <LayoutRoute />,
   },
   {
-    path: '/destination',
-    element: <DestinationInfo />
-  }
-])
+    path: "/destination",
+    element: <DestinationInfo />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-
     <RouterProvider router={router} />
   </React.StrictMode>
 );
