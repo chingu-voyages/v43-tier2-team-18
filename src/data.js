b9f87b7ff2798e206f32825931ca23163d89ac4f
Maikoke <mamadoumaikoke11@gmail.com>
2023-04-02 12:44:32 +0000

    travelData

--- src/data.js
+++ src/data.js
@@ -27,3 +27,307 @@ export const complimentaries = [
   "Free Wi-Fi in the vehicle during the tour.",
   "A small souvenir as a token of our appreciation for choosing our tour.",
 ];
+
+
+export const travelData = [
+  {
+    "destination": {
+      "name": "Japan",
+      "image": "https://cdn.pixabay.com/photo/2016/11/19/21/29/temple-1841296_1280.jpg",
+      "description": "Japan is a country with a fascinating blend of ancient traditions and modern technology. Its culture is deeply rooted in history and tradition, with a rich culinary tradition, beautiful gardens, and stunning temples and shrines. Japan is also known for its innovative technologies, bustling cities, and unique fashion.",
+      "popularCities": [
+       "Tokyo", "Kyoto", "Hiroshima", "Osaka"
+      ],
+      "attractions": [
+        {
+          "name": "Tokyo Skytree",
+          "images": "https://cdn.pixabay.com/photo/2017/08/07/13/43/building-2603950_1280.jpg"
+        },
+        {
+          "name": "Osaka Castle",
+          "images": "https://cdn.pixabay.com/photo/2020/08/10/01/28/castle-5476737_1280.jpg"
+        },
+        {
+          "name": "Kinkaku-j",
+          "images": "https://cdn.pixabay.com/photo/2019/02/02/07/00/kinkaku-ji-3970248_1280.jpg"
+        },
+        {
+          "name": "Hiroshima Peace Memorial Park",
+          "images": "https://cdn.pixabay.com/photo/2016/02/11/12/16/hiroshima-1193455_1280.jpg"
+        },
+      ]
+    }
+  },
+
+  {
+    "destination": {
+      "name": "New Zealand",
+      "image": "https://cdn.pixabay.com/photo/2013/03/04/20/59/valley-90388_1280.jpg",
+      "description": "New Zealand is a country of breathtaking natural beauty, with rugged mountain ranges, lush forests, and stunning coastlines. Its cities offer a lively cultural scene and delicious cuisine.",
+      "popularCities": [
+        "Auckland", "Queenstown", "Wellington", "Christchurch", 
+       
+      ],
+      "attractions": [
+        {
+          "name": "Sky Tower",
+          "images": "https://cdn.pixabay.com/photo/2013/01/01/15/56/new-zealand-73230__480.jpg"
+        },
+        {
+          "name": "Milford Sound",
+          "images": "https://cdn.pixabay.com/photo/2017/12/15/14/03/new-zealand-3021090__480.jpg"
+        },
+        {
+          "name": "Te Papa Museum",
+          "images": "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/be/32/36/museum-of-new-zealand.jpg?w=1200&h=-1&s=1"
+        },
+        {
+          "name": "International Antarctic Centre",
+          "images": "https://vrhotels.co.nz/parkview-on-hagley/wp-content/uploads/sites/13/2021/08/antarctic-centre.jpg"
+        },
+      ]
+    }
+  },
+
+  {
+    "destination": {
+      "name": "Peru",
+      "image": "https://cdn.pixabay.com/photo/2020/10/23/16/43/vicuna-5679264__480.jpg",
+      "description": "Peru is a country of ancient ruins, rich history, and stunning landscapes. Its cities offer a vibrant cultural scene and delicious cuisine, while its natural wonders draw visitors from around the world.",
+      "popularCities": [
+
+        "Lima", "Cusco", "Arequipa", "Iquitos"
+       
+      ],
+      "attractions": [
+        {
+          "name": "Machu Picchu",
+          "images": "https://cdn.pixabay.com/photo/2012/04/26/22/48/machu-picchu-43387__480.jpg"
+        },
+        {
+          "name": "Amazon Rainforest",
+          "images": "https://cdn.pixabay.com/photo/2014/02/24/17/45/lake-273783__480.jpg"
+        },
+        {
+          "name": "Museo Larco",
+          "images": "https://andareincentives.com/wp-content/uploads/2019/11/MG_1532-Museo-Larco-Cafe-Restaurant.jpg"
+        },
+        {
+          "name": "Huaca Pucllana",
+          "images": "https://cdn.pixabay.com/photo/2019/04/18/03/23/lima-4136013__480.jpg"
+        },
+      ]
+    }
+  },
+
+  {
+    "destination": {
+      "name": "Italy",
+      "image": "https://cdn.pixabay.com/photo/2015/11/27/20/28/cathedral-1066314__480.jpg",
+      "description": "Italy is a country of incredible architecture, art, and cuisine, with a rich history and diverse culture. Its cities offer a vibrant cultural scene and delicious food, while its natural beauty is stunning.",
+      "popularCities": [
+       "Rome", "Florence", "Venice", "Milan", 
+      ],
+      "attractions": [
+        {
+          "name": "Colosseum",
+          "images": "https://cdn.pixabay.com/photo/2020/11/24/19/34/colosseum-5773684__480.jpg"
+        },
+        {
+          "name": "Duomo",
+          "images": "https://cdn.pixabay.com/photo/2021/11/19/11/13/duomo-6808817__480.jpg"
+        },
+        {
+          "name": "Rialto Bridge",
+          "images": "https://cdn.pixabay.com/photo/2017/02/21/12/42/venice-2085864__480.jpg"
+        },
+        {
+          "name": "Milan Cathedral",
+          "images": "https://cdn.pixabay.com/photo/2018/07/08/10/26/italy-3523635__480.jpg"
+        },
+      ]
+    }
+  },
+
+
+  {
+    "destination": {
+      "name": "South Africa",
+      "image": "https://s30876.pcdn.co/wp-content/uploads/South-Africa.jpg.optimal.jpg",
+      "description": "South Africa is a country of stunning landscapes and diverse cultures, with a rich history and vibrant present. Its cities offer a lively cultural scene and delicious cuisine, while its natural wonders are breathtaking.",
+      "popularCities": [
+
+        "Cape Town", "Johannesburg", "Durban", "Pretoria"
+       
+      ],
+      "attractions": [
+        {
+          "name": "Table Mountain",
+          "images": "https://theexpeditionproject.com/wp-content/uploads/2018/07/8f0212609996c47b76a4037dd2d8d925.jpg"
+        },
+        {
+          "name": "Apartheid Museum",
+          "images": "https://www.apartheidmuseum.org/uploads/files/Exhibitions-images/Main-exhibition/_600xAUTO_crop_center-center/theme01-pillars_jpg01.jpg"
+        },
+        {
+          "name": "Union Buildings",
+          "images": "https://static.wheretostay.co.za/locations/poi/0/0/005613/525_xl.jpg"
+        },
+        {
+          "name": "Golden Mile",
+          "images": "https://www.south-african-hotels.com/media/the-durban-golden-mile-highlights-tour-6(1).jpg"
+        },
+      ]
+    }
+  },
+
+
+  {
+    "destination": {
+      "name": "Vietnam",
+      "image": "https://www.traveloffpath.com/wp-content/uploads/2023/01/Vietnam-Is-Becoming-More-Popular-Among-American-Tourists-Heres-Why.jpg",
+      "description": "Vietnam is a country of rich history and stunning natural landscapes. Its cuisine is famous for its fresh ingredients and unique flavors, and its cities offer a bustling and vibrant atmosphere.",
+      "popularCities": [
+       "Hanoi", "Ho Chi Minh City", "Da Nang", "Hoi An"
+      ],
+      "attractions": [
+        {
+          "name": "Hoan Kiem Lake",
+          "images": "https://cdn.pixabay.com/photo/2021/08/21/14/49/turtle-tower-6562995__480.jpg"
+        },
+        {
+          "name": "Cu Chi Tunnels",
+          "images": "https://statics.vinpearl.com/cu-chi-tunnels-9_1651073728.jpg"
+        },
+        {
+          "name": "My Son Sanctuary",
+          "images": "https://cdn.pixabay.com/photo/2017/08/20/13/23/vietnam-2661699__480.jpg"
+        },
+        {
+          "name": "Ancient Town",
+          "images": "https://vietnamdiscovery.com/wp-content/uploads/2019/09/Hoi-An-road-on-a-rainy-day.jpg"
+        },
+      ]
+    }
+  },
+
+  {
+    "destination": {
+      "name": "Australia",
+      "image": "https://www.state.gov/wp-content/uploads/2022/02/shutterstock_1025960785-2560x1300.jpg",
+      "description": "Australia is a country of diverse wildlife, stunning landscapes, and vibrant cities. Its beaches are world-renowned , and its cities offer a lively cultural scene and delicious cuisine.",
+      "popularCities": [
+        "Sydney", "Melbourne", "Brisbane", "Perth"
+      ],
+      "attractions": [
+        {
+          "name": "Sydney Opera House",
+          "images": "https://cdn.pixabay.com/photo/2018/05/07/22/08/opera-house-3381786__480.jpg"
+        },
+        {
+          "name": "Federation Square",
+          "images": "https://lp-cms-production.imgix.net/2019-06/5e82471b36716333af536a171a5d9bde-federation-square.jpg"
+        },
+        {
+          "name": "South Bank Parklands",
+          "images": "https://assets.website-files.com/5ebb5b046641ade72cccd980/60ac779278507dc179730406_000606_SthbankAerial_01-cmyk.jpg"
+        },
+        {
+          "name": "Rottnest Island",
+          "images": "https://aquariustraveller.com/wp-content/uploads/2021/07/P1100925.jpg"
+        },
+      ]
+    }
+  },
+
+  {
+    "destination": {
+      "name": "Iceland",
+      "image": "https://cdn.pixabay.com/photo/2017/06/24/19/45/flowers-2438618__480.jpg",
+      "description": " Iceland is a country of stunning natural beauty, with rugged landscapes, hot springs, and the famous Northern Lights. Its capital city, Reykjavik, offers a lively cultural scene, and the country is known for its fresh seafood and unique cuisine.",
+      "popularCities": [
+        "Reykjavik", "Akureyri", "Vik", "Husavik"
+       
+      ],
+      "attractions": [
+        {
+          "name": "Hallgrimskirkja",
+          "images": "https://cdn.pixabay.com/photo/2019/08/28/13/26/iceland-4436790__480.jpg"
+        },
+        {
+          "name": "Godafoss Waterfall",
+          "images": "https://cdn.pixabay.com/photo/2016/11/19/18/57/godafoss-1840758__480.jpg"
+        },
+        {
+          "name": "Reynisfjara Beach",
+          "images": "https://cdn.pixabay.com/photo/2020/03/30/20/31/vik-4985398__480.jpg"
+        },
+        {
+          "name": "Whale Museum",
+          "images": "https://cdn.pixabay.com/photo/2017/06/16/19/30/whale-2410095__480.jpg"
+        },
+      ]
+    }
+  },
+
+  {
+    "destination": {
+      "name": "Costa Rica",
+      "image": "https://cdn.pixabay.com/photo/2014/03/17/17/35/volcano-289253__480.jpg",
+      "description": "Costa Rica is a country of rich biodiversity, with lush rainforests, beautiful beaches, and stunning wildlife. It is known for its eco-tourism, with activities such as zip-lining and hiking through the jungle being popular.",
+      "popularCities": [
+       "San Jose", "Monteverde", "Manuel Antonio", "Puerto Viejo"
+      ],
+      "attractions": [
+        {
+          "name": "National Museum of Costa Rica",
+          "images": "https://planetofhotels.com/guide/sites/default/files/styles/paragraph__hero_banner__hb_image__1880bp/public/hero_banner/Museum_of_Art_of_Costa_Rica_0.jpg"
+        },
+        {
+          "name": "Monteverde Cloud Forest Reserve",
+          "images": "https://media.istockphoto.com/id/1349202863/photo/tourist-walking-on-a-suspension-bridge-in-monteverde-cloud-forest-costa-rica.jpg?b=1&s=170667a&w=0&k=20&c=ZDRlBMOmREH0LEU3yARCag9CC7AaE33nO3WUsqJXL_U="
+        },
+        {
+          "name": "Manuel Antonio National Park",
+          "images": "https://d2xuzatlfjyc9k.cloudfront.net/wp-content/uploads/2014/05/Manuel-Antonio-National-Park-1.jpg"
+        },
+        {
+          "name": "Cahuita National Park",
+          "images": "https://media.istockphoto.com/id/1357438148/photo/cahuita-national-park-costa-rica.jpg?b=1&s=170667a&w=0&k=20&c=OgY6vqh5xv3CNd7qzR0Pmt8ulJt94XHUfGtev47-1fM="
+        },
+      ]
+    }
+  },
+
+  {
+    "destination": {
+      "name": "United States",
+      "image": "https://cdn.pixabay.com/photo/2013/03/26/00/31/capitol-96827__480.jpg",
+      "description": "The United States is a country of diverse cultures , stunning natural landscapes, and world-famous landmarks. Its cities offer a lively cultural scene and delicious cuisine, while its national parks and natural wonders draw visitors from around the world.",
+      "popularCities": [
+        "New York City", "Los Angeles", "San Francisco", "Las Vegas"
+      ],
+      "attractions": [
+        {
+          "name": "Central Park",
+          "images": "https://cdn.pixabay.com/photo/2019/07/21/07/12/new-york-4352072__480.jpg"
+        },
+        {
+          "name": "Times Square",
+          "images": "https://cdn.pixabay.com/photo/2020/06/08/20/58/nyc-5276112__480.jpg"
+        },
+        {
+          "name": "Hollywood Walk of Fame",
+          "images": "https://cdn.pixabay.com/photo/2019/04/17/01/34/hollywood-4133248__480.jpg"
+        },
+        {
+          "name": "Broadway",
+          "images": "https://cdn.pixabay.com/photo/2013/06/08/21/26/usa-123420__480.jpg"
+        },
+      ]
+    }
+  },
+
+
+]
+  
