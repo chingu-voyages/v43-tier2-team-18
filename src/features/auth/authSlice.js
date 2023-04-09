75843f26c1ae8c2d25b129f150c34ab3107cabc8
NureniJamiu <nurenijamiu951@gmail.com>
2023-04-09 11:10:44 +0100

    add "loginCredentials" and "isLoggeIn" to state

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -4,14 +4,26 @@ const authSlice = createSlice({
   name: "auth",
   initialState: {
     signupCredentials: [],
+    loginCredentials: {},
+    isLoggedIn: false,
   },
   reducers: {
     // ...
     signupNewUser(state, action) {
+      const { email, password } = action.payload;
       state.signupCredentials.push(action.payload);
+      state.loginCredentials[email] = password;
+      state.isLoggedIn = "true";
     },
+
     clearValue(state) {
       state.signupCredentials = [];
+      state.loginCredentials = {};
+      state.isLoggedIn = false;
+    },
+
+    loginUser(state) {
+      state.isLoggedIn = true;
     },
   },
 });
