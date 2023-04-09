3c001af770ed338a3ef857648ba0ae495d8841bd
NureniJamiu <nurenijamiu951@gmail.com>
2023-04-09 11:09:00 +0100

    remove "blackList" from persistConfig

--- src/app/store.js
+++ src/app/store.js
@@ -1,14 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { reducer as authReducer } from "../features/auth/authSlice";
 
-//persistence in redux store
+//persistence to redux store
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
 const persistConfig = {
   key: "root",
   storage,
-  // blacklist: ["root"],
 };
 
 const persistedAuthReducer = persistReducer(persistConfig, authReducer);
