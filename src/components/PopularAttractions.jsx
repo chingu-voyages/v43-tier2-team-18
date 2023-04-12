4da3f790e116645e3b06ff042c6b50c7ea4923f8
akshkin <107682289+akshkin@users.noreply.github.com>
2023-04-12 19:00:00 +0200

    Hide scrollbar if there aren't enough cards in PopularAttractions

--- src/components/PopularAttractions.jsx
+++ src/components/PopularAttractions.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import PopularAttractionsCard from "./PopularAttractionsCard";
 
 function PopularAttractions({ destination }) {
+  const toShow = destination.attractions.length > 4;
+
   return (
     <div className="bg-heroBg bg-cover xs:p-6 md:p-8 md:px-12 lg:px-24">
       <h3 className="text-center font-bold text-4xl lg:text-5xl mb-10 mt-0">
         Popular Attractions
       </h3>
-      <div className="w-full flex flex-nowrap gap-8 pb-8 overflow-x-scroll  mt-8">
+      <div
+        className={`w-full flex flex-nowrap gap-8 pb-8 overflow-x-scroll  mt-8 ${
+          !toShow ? "xl:hide-scrollbar" : ""
+        }`}
+      >
         {destination.attractions.map((attraction) => (
           <PopularAttractionsCard
             key={attraction.name}
